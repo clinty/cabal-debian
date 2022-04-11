7ad4bfbc8e1dfd3fc1841ff188516fe0f761c943
Felix Lechner <felix.lechner@lease-up.com>
2022-04-11 16:28:31 -0700

    Use Debhelper's dh sequencer in the default d/rules.

--- src/Debian/Debianize/Finalize.hs
+++ src/Debian/Debianize/Finalize.hs
@@ -747,8 +747,8 @@ finalizeRules =
        (A.debInfo . D.rulesSettings) %= (++ ["DEB_DEFAULT_COMPILER = " <> pack hcdeb])
        flags <- flagString . Set.toList <$> use (A.debInfo . D.flags . cabalFlagAssignments)
        unless (List.null flags) ((A.debInfo . D.rulesSettings) %= (++ ["DEB_SETUP_GHC_CONFIGURE_ARGS = " <> pack flags]))
-       (A.debInfo . D.rulesIncludes) %= (++ ["include /usr/share/cdbs/1/rules/debhelper.mk",
-                                             "include /usr/share/cdbs/1/class/hlibrary.mk"])
+       (A.debInfo . D.rulesIncludes) %= (++ ["%:",
+                                             "\tdh $@"])
 
 data Dependency_
   = BuildDepends Dependency
