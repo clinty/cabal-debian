5b24de45aa3b93928503feb9715760be142a1b59
Clint Adams <clint@debian.org>
2022-06-17 09:30:40 -0400

    Set Standards-Version to 4.6.1

--- src/Debian/Debianize/Optparse.hs
+++ src/Debian/Debianize/Optparse.hs
@@ -354,7 +354,7 @@ standardsVersionP :: O.Parser StandardsVersion
 standardsVersionP = O.option (parseStandardsVersion <$> O.str) m where
   m = O.help helpMsg
       <> O.long "standards-version"
-      <> O.value (parseStandardsVersion "4.5.0")
+      <> O.value (parseStandardsVersion "4.6.1")
       <> O.metavar "CABALVERSION"
   helpMsg = unlines [
     "Claim compatibility to this version of the Debian policy",
