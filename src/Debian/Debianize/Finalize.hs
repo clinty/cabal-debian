cfdbb94415d25c4dd07082d3861f28672fecddbf
Clint Adams <clint@debian.org>
2026-08-22 14:26:44 -0400

    feat: update to v5 watchfile syntax

--- src/Debian/Debianize/Finalize.hs
+++ src/Debian/Debianize/Finalize.hs
@@ -154,7 +154,7 @@ finalizeDebianization' goodies date currentUser debhelperCompat =
 
 watchAtom :: PackageName -> Text
 watchAtom pkgname =
-    pack $ "version=4\nhttps://hackage.haskell.org/package/" ++ unPackageName pkgname ++ "/distro-monitor .*-([0-9\\.]+)\\.(?:zip|tgz|tbz|txz|(?:tar\\.(?:gz|bz2|xz)))\n"
+    pack $ "Version: 5\n\nSource: https://hackage.haskell.org/package/" ++ unPackageName pkgname ++ "/distro-monitor\nMatching-Pattern: .*-([0-9\\.]+).(?:zip|tgz|tbz|txz|(?:tar\\.(?:gz|bz2|xz)))\n"
 
 -- | Compute the final values of the BinaryDebDescription record
 -- description fields from the cabal descriptions and the values that
